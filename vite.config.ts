eefcf4fd8de8dab914edcacb53ddf563ab6c189f
annis94 <anis561@gmail.com>
2025-07-30 17:56:00 +0200

    Configuration déploiement GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Axel-Service/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
